e2c60c7543424a8463c8d3183490943d34121fd9
Alex Virlan <dj.3agl3@gmail.com>
2024-03-13 22:36:35 +0200

    New UI - main window

--- SimpleSendKeys/Forms/frmMain.cs
+++ SimpleSendKeys/Forms/frmMain.cs
@@ -1,13 +1,28 @@
-namespace SimpleSendKeys
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleSendKeys.Forms
 {
-    public partial class Form1 : Form
+    public partial class frmMain : Form
     {
-        public Form1()
+        public frmMain()
         {
             InitializeComponent();
         }
 
-        private void btnSendKeys_Click(object sender, EventArgs e)
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TestVoid(object sender, EventArgs e)
         {
             Thread.Sleep(2500);
             //SendKeys.SendWait("A");
@@ -15,7 +30,7 @@ namespace SimpleSendKeys
             //SendKeys.SendWait(" ");
             //SendKeys.SendWait(".");
 
-            foreach (char c in txtText.Text)
+            foreach (char c in "Test")
             {
                 if (c == ' ') { SendKeys.SendWait(" "); }
                 else { SendKeys.SendWait($"{{{c}}}"); }
