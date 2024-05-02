308e9dd73fed5c82906af9db794c80696ae72bf1
Alex Virlan <dj.3agl3@gmail.com>
2024-05-02 13:32:59 +0300

    Update startup

--- SimpleSendKeys/Program.cs
+++ SimpleSendKeys/Program.cs
@@ -1,25 +1,47 @@
 using SimpleSendKeys.Forms;
 using SimpleSendKeys.Utils;
+using System.Reflection;
+using System.Runtime.InteropServices;
 
 namespace SimpleSendKeys
 {
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
+        #region Variables and dll Imports
+        private const int HWND_BROADCAST = 0xFFFF;
+        private static readonly int WM_SSK_SHOW = RegisterWindowMessage("WM_SSK_SHOW");
+        private static readonly int WM_SSK_SEND = RegisterWindowMessage("WM_SSK_SEND");
+
+        [DllImport("user32")]
+        private static extern bool PostMessage(IntPtr hwnd, int msg, IntPtr wparam, IntPtr lparam);
+
+        [DllImport("user32")]
+        private static extern int RegisterWindowMessage(string message);
+        #endregion
+
         [STAThread]
         static void Main()
         {
-            Mutex mutex = new Mutex(true, "SimpleSendKeys", out bool newRun);
-            if (!newRun) { return; }
 
-            ApplicationConfiguration.Initialize();
 
-            List<string>? args = Environment.GetCommandLineArgs().Skip(1).ToList();
-            frmMain frmMain = new(args);
-            frmMain.Show();
-            Application.Run();
+            List<string> args = Environment.GetCommandLineArgs().Skip(1).ToList();
+            args.ForEach(arg => arg = arg.Replace("-", string.Empty));
+
+            Mutex mutex = new Mutex(true, "AlexVirlan.SimpleSendKeys", out bool newInstance);
+            if (newInstance)
+            {
+                ApplicationConfiguration.Initialize();
+                frmMain frmMain = new(args);
+                frmMain.Show();
+                Application.Run();
+            }
+            else
+            {
+                int wmMessage = args.Contains("send", StringComparer.OrdinalIgnoreCase) ? WM_SSK_SEND : WM_SSK_SHOW;
+                PostMessage((IntPtr)HWND_BROADCAST, wmMessage, new IntPtr(0xCDCD), new IntPtr(0xEFEF));
+                Environment.Exit(0);
+            }
+            mutex.ReleaseMutex();
         }
 
         static Program()
